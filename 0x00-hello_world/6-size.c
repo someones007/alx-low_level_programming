ef39dee2c24e6553ed1057f21e242204bf95c351
someones007 <143654731+someones007@users.noreply.github.com>
2023-09-08 04:58:15 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-    printf("Size of a char: %d byte(s)\n"
-           "Size of an int: %d byte(s)\n"
-           "Size of a long int: %d byte(s)\n"
-           "Size of a long long int: %d byte(s)\n"
-           "Size of a float: %d byte(s)\n",
-           sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
-    return (0);
+         printf("Size of a char: %d byte(s)\n"
+         "Size of an int: %d byte(s)\n"
+         "Size of a long int: %d byte(s)\n"
+         "Size of a long long int: %d byte(s)\n"
+         "Size of a float: %d byte(s)\n",
+         sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+         return (0);
 }
