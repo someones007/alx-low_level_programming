20322a9855c6a91a23737f98588c073886da6227
someones007 <143654731+someones007@users.noreply.github.com>
2023-09-08 03:39:53 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Prints a text
+ * Return: Zero
+ */
+
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
