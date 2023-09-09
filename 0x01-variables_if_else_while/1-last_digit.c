4a4af813ef976b38ed479ecf442a11ca0bcdcc68
someones007 <143654731+someones007@users.noreply.github.com>
2023-09-09 05:15:53 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+
 
 /**
  * main - main function
@@ -9,7 +9,7 @@
  * Return: zero
  */
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
