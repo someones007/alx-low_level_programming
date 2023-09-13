48fb47674be7ad755bd22f3e70d8f8a5b446c74f
someones007 <143654731+someones007@users.noreply.github.com>
2023-09-13 05:05:00 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * main - Entry point
+ * Description: Prints "_putchar" followed by a new line.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+        char varr[] = "_putchar";
+        int i = 0;
+
+        while (varr[i] != '\0')
+        {
+		_putchar(varr[i]);
+		i++;
+        }
+        _putchar('\n');
+
+        return (0);
+}
