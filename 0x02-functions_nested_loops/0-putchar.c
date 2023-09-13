00854adc5944d1ee31fdcd9ed188c11991484d63
someones007 <143654731+someones007@users.noreply.github.com>
2023-09-13 05:06:49 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-        char varr[] = "_putchar";
-        int i = 0;
+	char varr[] = "_putchar";
+	int i = 0;
 
-        while (varr[i] != '\0')
-        {
+	while (varr[i] != '\0')
+	{
 		_putchar(varr[i]);
 		i++;
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
